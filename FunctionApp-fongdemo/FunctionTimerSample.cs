286c307071bd420394477683ed3fe5e1a77df2b0
fong9931 <fong9931@hotmail.com>
2019-10-10 10:21:59 +0800

    new timer functions

---
+++ FunctionApp-fongdemo/FunctionTimerSample.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace FunctionApp_fongdemo
+{
+    public static class FunctionTimerSample
+    {
+        [FunctionName("FunctionTimerSample")]
+        public static async Task Run([TimerTrigger("0 */5 * * * *")]TimerInfo myTimer, ILogger log)
+        {
+            log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
+        }
+    }    
+}
