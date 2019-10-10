ab1d9a82b7d9567092f4e57933611d366b763ff5
fong9931 <fong9931@hotmail.com>
2019-10-10 10:23:22 +0800

    add test httprequest

--- FunctionApp-fongdemo/FunctionTimerSample.cs
+++ FunctionApp-fongdemo/FunctionTimerSample.cs
@@ -17,6 +17,30 @@ namespace FunctionApp_fongdemo
         public static async Task Run([TimerTrigger("0 */5 * * * *")]TimerInfo myTimer, ILogger log)
         {
             log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
+            var url = "";
+            var results = await SendRequest(url);
+            log.LogInformation(String.Format("AssetCreateModels: {0}", results));
+
         }
-    }    
+
+        public static async Task<string> SendRequest(string url)
+        {
+            using (var client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic",
+                Convert.ToBase64String(Encoding.ASCII.GetBytes($"{Environment.GetEnvironmentVariable("Auth_User")}:{Environment.GetEnvironmentVariable("Auth_Pass")}")));
+                Dictionary<string, string> dictionary = new Dictionary<string, string>();
+                //dictionary.Add("PARAM1", "VALUE1");
+                //dictionary.Add("PARAM2", text);
+
+                string json = JsonConvert.SerializeObject(dictionary);
+                var requestData = new StringContent(json, Encoding.UTF8, "application/json");
+
+                var response = await client.PostAsync(url, requestData);
+                var result = await response.Content.ReadAsStringAsync();
+
+                return result;
+            }
+        }
+    }
 }
